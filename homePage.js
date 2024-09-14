c09db4324336c600aec4601ed8f6ef2d5f9a28f0
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2024-09-14 21:36:02 +0500

    savdo platform

--- homePage.js
+++ homePage.js
@@ -11,22 +11,70 @@ fetch("https://api-5-lac.vercel.app/")
   .then((data) => {
     // Ma'lumotni olish muvaffaqiyatli bo'lganda
     const usersTableBody = document.querySelector("#usersTable tbody");
-    const users = data.data; // API javobidan foydalanuvchilarni olamiz
+    const users = data.data; 
 
     // Har bir foydalanuvchini jadvalga qo'shamiz
     users.forEach((user) => {
       const row = document.createElement("tr");
       row.innerHTML = `
         <td>${user.id}</td>
-        <td>${user.fullName}</td>
+        <td>${user.fullname}</td>
         <td>${user.email}</td>
-        <td>${user.userName}</td>
+        <td>${user.username}</td>
         <td>${user.password}</td>
-        <td>${user.phoneNumber}</td>
+        <td>${user.phonenumber}</td>
       `;
       usersTableBody.appendChild(row);
     });
   })
   .catch((error) => {
     console.error("Xatolik:", error);
+  });
+
+// POST so'rovini yuborish
+document
+  .getElementById("userForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Formani yuborishni oldini oladi
+
+    const formData = {
+      fullname: this.fullname.value,
+      email: this.email.value,
+      password: this.password.value,
+      username: this.username.value,
+      phonenumber: this.phonenumber.value,
+    };
+
+    // API'ga POST so'rovi yuborish
+    fetch("https://api-5-lac.vercel.app/user/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Serverda xatolik yuz berdi");
+        }
+      })
+      .then((data) => {
+        // Javobdagi ma'lumotni jadvallingizga qo'shasiz
+        const usersTableBody = document.querySelector("#usersTable tbody");
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td>${data.data.id}</td>
+        <td>${data.data.fullname}</td>
+        <td>${data.data.email}</td>
+        <td>${data.data.username}</td>
+        <td>${data.data.password}</td>
+        <td>${data.data.phonenumber}</td>
+      `;
+        usersTableBody.appendChild(row);
+      })
+      .catch((error) => {
+        console.error("Xatolik:", error);
+      });
   });
